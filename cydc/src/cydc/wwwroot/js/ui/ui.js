163d2dbfe853c21f05446b1353705da19f614d00
文旺 <gray.wen@outlook.com>
2015-10-15 13:39:05 +0800

    +UI

---
+++ cydc/src/cydc/wwwroot/js/ui/ui.js
@@ -0,0 +1,9 @@
+var Ui;
+(function (Ui) {
+    var Indexpage = (function () {
+        function Indexpage() {
+        }
+        return Indexpage;
+    })();
+    Ui.Indexpage = Indexpage;
+})(Ui || (Ui = {}));
---
+++ cydc/src/cydc/wwwroot/js/ui/ui.ts
@@ -0,0 +1,5 @@
+﻿module Ui {
+    export class Indexpage {
+        
+    }
+}
