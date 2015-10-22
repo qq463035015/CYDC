160a86e10972cde61c2882df0559ab492ccbd8f6
flysha <flysha@live.com>
2015-10-22 16:55:31 +0800

    +example

--- cydc/src/cydc/wwwroot/ui/js/viewModels/shell.js
+++ cydc/src/cydc/wwwroot/ui/js/viewModels/shell.js
@@ -7,7 +7,8 @@ define(["require", "exports", 'plugins/router', 'knockout'], function (require, 
         viewModel.prototype.activate = function () {
             router.makeRelative({ moduleId: 'viewModels' });
             router.map([
-                { route: ['', 'home/index'], moduleId: 'home/index', title: 'home', nav: true, hash: '#home/index' }
+                { route: ['', 'home/index'], moduleId: 'home/index', title: 'home', nav: true, },
+                { route: 'home/hello', moduleId: 'home/hello', title: 'hello', nav: true }
             ]).buildNavigationModel();
             return router.activate({ pushState: true, root: '/ui' });
         };
