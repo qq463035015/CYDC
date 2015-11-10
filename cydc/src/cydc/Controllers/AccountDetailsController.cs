ae3609b42061b8b21ab1b681df033671a34a7757
文旺 <gray.wen@outlook.com>
2015-11-10 17:26:32 +0800

    添加accoutDetails foodOrder api

---
+++ cydc/src/cydc/Controllers/AccountDetailsController.cs
@@ -0,0 +1,36 @@
+﻿using cydc.Models;
+using Microsoft.AspNet.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cydc.Controllers
+{
+    public class AccountDetailsController : Controller
+    {
+        private readonly ApplicationDbContext _adc;
+
+        public async Task<object> List(AccountDetailsQuery query)
+        {
+            IQueryable<AccountDetails> data = _adc.AccountDetails;
+            if (query.UserId != null)
+            {
+                data = data.Where(x => x.UserId == query.UserId);
+            }
+            return await data.CreatePagedList(query);
+        }
+
+        public async Task<int> Add(string userId, decimal account)
+        {
+            AccountDetails accountDetail = new AccountDetails
+            {
+                UserId = userId,
+                Amount = account,
+                CreateTime = DateTime.Now
+            };
+            _adc.Add(accountDetail);
+            return await _adc.SaveChangesAsync();
+        }
+    }
+}
---
+++ cydc/src/cydc/Controllers/FoodOrderPaymentController.cs
@@ -0,0 +1,31 @@
+﻿using cydc.Models;
+using Microsoft.AspNet.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cydc.Controllers
+{
+    public class FoodOrderPaymentController : Controller
+    {
+        private readonly ApplicationDbContext _adc;
+
+        public async Task<object> List(FoodOrderPaymentQuery query)
+        {
+            return await _adc.FoodOrderPayments.CreatePagedList(query);
+        }
+
+        public async Task<object> Add()
+        {
+            
+            FoodOrderPayment foodOrserPayment = new FoodOrderPayment
+            {
+                
+            };
+
+            _adc.Add(foodOrserPayment);
+            return await _adc.SaveChangesAsync();
+        }
+    }
+}
