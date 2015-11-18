b1fc41636bb2f9df1ee0f777c60acf73f56e38a4
Flysha <sdflysha@qq.com>
2015-11-18 17:24:18 +0800

    二级菜单

--- cydc/src/cydc/wwwroot/ui/js/viewModels/home/menu.ts
+++ cydc/src/cydc/wwwroot/ui/js/viewModels/home/menu.ts
@@ -1,5 +1,21 @@
-﻿class viewModel {
+﻿import router = require('plugins/router');
 
+class viewModel {
+    router: DurandalRootRouter;
+
+    constructor() {
+        window['vm'] = this;
+        this.router = router.createChildRouter()
+            .makeRelative({
+                moduleId: 'viewModels/home/menu',
+                fromParent: true
+            }).map([
+                { route: ['', 'index'], moduleId: 'index', title: '菜谱管理', nav: true },
+                { route: 'type', moduleId: 'type', title: '菜谱类型管理', nav: true },
+                { route: 'address', moduleId: 'address', title: '地点管理', nav: true },
+                { route: 'notice', moduleId: 'notice', title: '通知管理', nav: true },
+            ]).buildNavigationModel();
+    }
 }
 
 export = new viewModel();
