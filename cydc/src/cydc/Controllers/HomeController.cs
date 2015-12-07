96d5da0def1d77b62f3eda06f3750893f69aaf6a
Flysha <sdflysha@qq.com>
2015-12-07 16:51:24 +0800

    +ENV test

--- cydc/src/cydc/Controllers/HomeController.cs
+++ cydc/src/cydc/Controllers/HomeController.cs
@@ -3,14 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
+using Microsoft.AspNet.Hosting;
 
 namespace cydc.Controllers
 {
     public class HomeController : CydcBaseController
     {
+        [FromServices]
+        public IHostingEnvironment HostingEnvironment { get; set; }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult ENV()
+        {
+            return Content(HostingEnvironment.EnvironmentName);
         }
     }
 }
