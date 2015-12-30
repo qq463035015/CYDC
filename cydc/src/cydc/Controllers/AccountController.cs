7de17b5d42bb0356f38426f58c9c66e7e5e9b911
gray-wen <gray.wen@outlook.com>
2015-12-30 14:05:08 +0800

    + ExitSign

--- cydc/src/cydc/Controllers/AccountController.cs
+++ cydc/src/cydc/Controllers/AccountController.cs
@@ -136,5 +136,10 @@ namespace cydc.Controllers
 
             return null;
         }
+
+        public async void ExitSign()
+        {
+            await _signInManager.SignOutAsync();
+        }
     }
 }
