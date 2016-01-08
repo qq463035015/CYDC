173e6c71e17f74bd4f1a0d0242601bfaa803cc36
flysha <flysha@live.com>
2016-01-08 13:05:29 +0800

    +install dnvm

--- cydc/src/cydc/Properties/PublishProfiles/周杰-阿里云-发布脚本.bat
+++ cydc/src/cydc/Properties/PublishProfiles/周杰-阿里云-发布脚本.bat
@@ -1,5 +1,8 @@
 REM 周杰_阿里云_发布脚本.bat
 
+REM Install DNVM
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "&{$Branch='dev';$wc=New-Object System.Net.WebClient;$wc.Proxy=[System.Net.WebRequest]::DefaultWebProxy;$wc.Proxy.Credentials=[System.Net.CredentialCache]::DefaultNetworkCredentials;Invoke-Expression ($wc.DownloadString('https://raw.githubusercontent.com/aspnet/Home/dev/dnvminstall.ps1'))}"
+
 REM 列出并使用.NET版本
 dnvm install 1.0.0-rc1-update1
 dnvm list
