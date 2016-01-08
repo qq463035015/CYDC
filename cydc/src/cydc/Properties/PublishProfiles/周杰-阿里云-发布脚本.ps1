3418e3866f89fccd1d486e5e3627946b7e271099
flysha <flysha@live.com>
2016-01-08 13:10:15 +0800

    reuse ps

---
+++ cydc/src/cydc/Properties/PublishProfiles/周杰-阿里云-发布脚本.ps1
@@ -0,0 +1,29 @@
+# 周杰_阿里云_发布脚本.bat
+
+# Install DNVM
+&{
+	$Branch='dev';
+	$wc=New-Object System.Net.WebClient;
+	$wc.Proxy=[System.Net.WebRequest]::DefaultWebProxy;
+	$wc.Proxy.Credentials=[System.Net.CredentialCache]::DefaultNetworkCredentials;
+	Invoke-Expression ($wc.DownloadString('https://raw.githubusercontent.com/aspnet/Home/dev/dnvminstall.ps1'))
+}
+
+# 列出并使用.NET版本
+&"C:\Windows\ServiceProfiles\LocalService\.dnx\bin\dnvm.cmd" install 1.0.0-rc1-update1
+&"C:\Windows\ServiceProfiles\LocalService\.dnx\bin\dnvm.cmd" list
+&"C:\Windows\ServiceProfiles\LocalService\.dnx\bin\dnvm.cmd" use 1.0.0-rc1-update1
+
+# 更新库程序包
+cd cydc/src/cydc
+dnu restore
+
+# 强制关闭当前正在运行的dnx.exe，通过进程名、IIS应用程序池的身份来识别
+taskkill /f /fi "imagename eq dnx.exe" /fi "username eq cydc"
+
+# 发布至根目录的o文件夹
+dnu publish -o C:\state\web\cydc-demo
+
+# 列出o文件夹的内容
+cd C:\state\web\cydc-demo
+ls
