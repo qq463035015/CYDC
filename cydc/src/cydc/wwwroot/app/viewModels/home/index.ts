26309d20234c0e0e1b88594846ecba7034fe2b29
flysha <flysha@live.com>
2016-01-08 17:56:04 +0800

    fix the fucking bug

--- cydc/src/cydc/wwwroot/app/viewModels/home/index.ts
+++ cydc/src/cydc/wwwroot/app/viewModels/home/index.ts
@@ -58,8 +58,9 @@ class viewModel {
     commitOrder() {
         if (auth.authed()) {
             var now = moment().format('YYYY-MM-DD HH:mm:ss');
-            var time = moment().format('YYYY-MM-DD 10:30:00');
-            if (!(now < time || (time > now && now < moment().format('YYYY-MM-DD 18:00:00')))) {
+            var morning = moment().format('YYYY-MM-DD 10:30:00');
+            let afternoon = moment().format('YYYY-MM-DD 24:00:00');
+            if (!((now < morning) || (now < afternoon))) {
                 utils.confirm('', '超过点餐时间,请联系管理员！').then(cs=> cs.close());
                 $('#modal-sample').modal('hide');
                 return null;
