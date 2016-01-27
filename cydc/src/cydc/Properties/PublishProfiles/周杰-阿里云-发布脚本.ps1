e1dda533cda1de3edff537db63de8b7725924e5d
flysha <flysha@live.com>
2016-01-27 14:18:19 +0800

    restore publish script

--- cydc/src/cydc/Properties/PublishProfiles/周杰-阿里云-发布脚本.ps1
+++ cydc/src/cydc/Properties/PublishProfiles/周杰-阿里云-发布脚本.ps1
@@ -14,13 +14,12 @@ dnvm install 1.0.0-rc1-update1 -arch x64
 dnvm list
 dnvm use 1.0.0-rc1-update1 -arch x64
 
+# 更新库程序包
+cd cydc/src/cydc
+dnu restore
+
 # 强制关闭当前正在运行的dnx.exe，通过进程名、IIS应用程序池的身份来识别
 taskkill /f /fi "imagename eq dnx.exe" /fi "username eq cydc"
-
-# 更新库程序包
-# 如果没什么好更新的，就可以注释这一步
-cd cydc/src/cydc
-# dnu restore
 
 # 发布至根目录的o文件夹
 dnu publish --runtime active -o C:\state\web\cydc-demo
