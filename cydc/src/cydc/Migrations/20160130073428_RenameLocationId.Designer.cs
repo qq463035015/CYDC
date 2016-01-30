a73a0165bf51ad5bec786c786ba4db47abce65b6
flysha <flysha@live.com>
2016-01-30 15:39:41 +0800

    +LocationIdRename migration

--- cydc/src/cydc/Migrations/20160130073428_RenameLocationId.Designer.cs
+++ cydc/src/cydc/Migrations/20160130073428_RenameLocationId.Designer.cs
@@ -8,9 +8,10 @@ using cydc.Models;
 namespace cydc.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20160130073428_RenameLocationId")]
+    partial class RenameLocationId
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "7.0.0-rc1-16348")
