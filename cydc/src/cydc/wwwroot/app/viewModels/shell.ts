b04716294f372b5728c7f905d57da60302b959d0
flysha <flysha@live.com>
2016-02-01 15:01:06 +0800

    fix https://github.com/Gray-wen/CYDC/issues/10

--- cydc/src/cydc/wwwroot/app/viewModels/shell.ts
+++ cydc/src/cydc/wwwroot/app/viewModels/shell.ts
@@ -13,12 +13,12 @@ class viewModel {
     activate() {
         router.makeRelative({ moduleId: 'viewModels' });
         router.map([
-            { route: ['', 'home/index'], moduleId: 'home/index', title: '首页', nav: true, },
-            { route: 'home/record', moduleId: 'home/record', title: '点餐记录', nav: true },
-            { route: 'home/user', moduleId: 'home/user', title: '用户管理', nav: true },
-            { route: 'home/menu', moduleId: 'home/menu', title: '菜谱管理', nav: true, hasChildRoutes: true },
-            { route: 'home/bill', moduleId: 'home/bill', title: '账单管理', nav: true },
-            { route: 'home/order', moduleId: 'home/order', title: '点餐列表', nav: true },
+            { route: ['', 'home/index'], moduleId: 'home/index', title: '首页', nav: true, visible: true},
+            { route: 'home/record', moduleId: 'home/record', title: '点餐记录', nav: true, visible: true },
+            { route: 'home/user', moduleId: 'home/user', title: '用户管理', nav: true, visible: auth.isAdmin },
+            { route: 'home/menu', moduleId: 'home/menu', title: '菜谱管理', nav: true, hasChildRoutes: true, visible: auth.isAdmin },
+            { route: 'home/bill', moduleId: 'home/bill', title: '账单管理', nav: true, visible: auth.isAdmin },
+            { route: 'home/order', moduleId: 'home/order', title: '点餐列表', nav: true, visible: auth.isAdmin },
             { route: 'account/login', moduleId: 'account/login', title: '登录', nav: false },
             { route: 'account/register', moduleId: 'account/register', title: '注册', nav: false },
             { route: 'account/changePassword', moduleId: 'account/changePassword', title: '修改密码', nav: false }
