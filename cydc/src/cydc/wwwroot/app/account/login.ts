26121d0698b71d857699beacf7012923cc2d378c
flysha <flysha@live.com>
2016-03-04 14:37:18 +0800

    +missing login.ts

---
+++ cydc/src/cydc/wwwroot/app/account/login.ts
@@ -0,0 +1,24 @@
+﻿namespace Cydc.Account {
+    export class LoginCtrl {
+        static $inject = ["auth", "$mdToast", "$location"];
+        constructor(public auth: Auth, public mdToast: ng.material.IToastService, public location: ng.ILocationService) {
+        }
+
+        userName: string;
+        password: string;
+        rememberMe = true;
+
+        requesting = false;
+
+        login() {
+            this.requesting = true;
+            this.auth.login(this.userName, this.password).then(() => {
+                this.location.path("/");
+            }).catch((r) => {
+                this.mdToast.showSimple("Username or password not correct.");
+            }).finally(() => {
+                this.requesting = false;
+            });
+        }
+    }
+}
