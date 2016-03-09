33dfb7fe9259db4858009c7988676280512bb288
lilbear.tang <845556405@qq.com>
2016-03-09 17:51:00 +0800

    modify order

--- cydc/src/cydc/wwwroot/app/foodOrder/order.js
+++ cydc/src/cydc/wwwroot/app/foodOrder/order.js
@@ -4,7 +4,71 @@ var Cydc;
     (function (FoodOrder) {
         var OrderCtrl = (function () {
             function OrderCtrl() {
+                this.tasteSelected = 0;
+                this.locationSelected = 0;
+                this.sel = {
+                    group1: 'Banana',
+                    group2: '2',
+                    menu: '1'
+                };
+                this.menu = [{
+                        id: "1",
+                        price: '12',
+                        title: '红萝卜炒肉、腊鱼、腐竹、包菜',
+                        value: '1'
+                    }, {
+                        id: "2",
+                        price: '15',
+                        title: '土豆红烧肉、大祘炒蛋、小菜、下饭菜',
+                        value: '2'
+                    }];
+                this.name = "John Doe";
+                this.comment = "";
+                this.taste0 = true;
+                this.taste1 = false;
+                this.location0 = true;
+                this.location1 = false;
+                this.location2 = false;
+                this.taste = ["香辣", "清淡"];
+                this.location = ["辰运", "蜜獾", "易观国际"];
+                this.selMenu = 0;
+                this.visible = false;
             }
+            OrderCtrl.prototype.updateTasteSelected = function (selType) {
+                if (selType == 1) {
+                    this.taste0 = false;
+                    this.taste1 = true;
+                }
+                else {
+                    this.taste0 = true;
+                    this.taste1 = false;
+                }
+                this.tasteSelected = selType;
+            };
+            ;
+            OrderCtrl.prototype.updateLocation = function (sel) {
+                if (sel == 0) {
+                    this.location0 = true;
+                    this.location1 = false;
+                    this.location2 = false;
+                }
+                else if (sel == 1) {
+                    this.location0 = false;
+                    this.location1 = true;
+                    this.location2 = false;
+                }
+                else if (sel == 2) {
+                    this.location0 = false;
+                    this.location1 = false;
+                    this.location2 = true;
+                }
+                this.locationSelected = sel;
+            };
+            ;
+            OrderCtrl.prototype.submit = function () {
+                this.visible = !this.visible;
+            };
+            OrderCtrl.$inject = ["$timeout"];
             return OrderCtrl;
         }());
         FoodOrder.OrderCtrl = OrderCtrl;
--- cydc/src/cydc/wwwroot/app/foodOrder/order.ts
+++ cydc/src/cydc/wwwroot/app/foodOrder/order.ts
@@ -1,5 +1,75 @@
 ﻿namespace Cydc.FoodOrder {
     export class OrderCtrl {
+        static $inject = ["$timeout"];
+        tasteSelected = 0;
+        locationSelected = 0;
+       
+
+        updateTasteSelected(selType) {
+            if (selType == 1) {
+                this.taste0 = false;
+                this.taste1 = true;
+            } else {
+                this.taste0 = true;
+                this.taste1 = false;
+            }
+            this.tasteSelected = selType;
+        };
+
+        sel = {
+            group1: 'Banana',
+            group2: '2',
+            menu: '1'
+        };
+
+        menu = [{
+            id: "1",
+            price: '12',
+            title: '红萝卜炒肉、腊鱼、腐竹、包菜',
+            value: '1'
+        }, {
+                id: "2",
+                price: '15',
+                title: '土豆红烧肉、大祘炒蛋、小菜、下饭菜',
+                value: '2'
+            }];
+
+
+        updateLocation(sel) {
+            if (sel == 0) {
+                this.location0 = true;
+                this.location1 = false;
+                this.location2 = false;
+            } else if (sel == 1) {
+                this.location0 = false;
+                this.location1 = true;
+                this.location2 = false;
+            } else if (sel == 2) {
+                this.location0 = false;
+                this.location1 = false;
+                this.location2 = true;
+            }
+            this.locationSelected = sel;
+        };
+
+        name = "John Doe";
+        comment = "";
+        taste0 = true;
+        taste1 = false;
+        location0 = true;
+        location1 = false;
+        location2 = false;
+        taste = ["香辣", "清淡"]
+        location = ["辰运", "蜜獾", "易观国际"]
+        selMenu = 0;
+
+        visible = false;
+        submit  () {
+            this.visible = !this.visible;
+        }
+
+
+
 
     }
 }
