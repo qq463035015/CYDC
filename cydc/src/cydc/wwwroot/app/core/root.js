ab59615e7239c4809d826d51d43935c2f6f05a8d
flysha <flysha@live.com>
2016-03-11 11:16:19 +0800

    splite root and core

---
+++ cydc/src/cydc/wwwroot/app/core/root.js
@@ -0,0 +1,54 @@
+var Cydc;
+(function (Cydc) {
+    var Core;
+    (function (Core) {
+        var RootCtrl = (function () {
+            function RootCtrl(auth, $location, $mdSidenav, $mdDialog) {
+                this.auth = auth;
+                this.$location = $location;
+                this.$mdSidenav = $mdSidenav;
+                this.$mdDialog = $mdDialog;
+                this.auth.refreshState();
+                window["root"] = this;
+            }
+            RootCtrl.prototype.authState = function () {
+                return this.auth;
+            };
+            RootCtrl.prototype.login = function ($event) {
+                return this.$mdDialog.show({
+                    controller: Cydc.Account.LoginCtrl,
+                    controllerAs: "vm",
+                    templateUrl: "/view/account/login.html",
+                    parent: angular.element(document.body),
+                    targetEvent: $event,
+                    clickOutsideToClose: true
+                });
+            };
+            RootCtrl.prototype.logout = function ($event) {
+                var _this = this;
+                this.$mdDialog.show(this.$mdDialog.confirm()
+                    .parent(angular.element(document.body))
+                    .clickOutsideToClose(true)
+                    .title("确定要退出吗？")
+                    .ariaLabel("Logout Dialog")
+                    .targetEvent($event)
+                    .ok("确定")
+                    .cancel("取消")).then(function () {
+                    return _this.auth.logout();
+                }).then(function () {
+                    _this.$mdDialog.show(_this.$mdDialog.alert()
+                        .title("退出成功")
+                        .targetEvent($event)
+                        .ariaLabel("Logout Success")
+                        .ok("知道了"));
+                });
+            };
+            RootCtrl.prototype.openMenu = function () {
+                this.$mdSidenav("left").toggle();
+            };
+            RootCtrl.$inject = ["auth", "$location", "$mdSidenav", "$mdDialog"];
+            return RootCtrl;
+        }());
+        Core.RootCtrl = RootCtrl;
+    })(Core = Cydc.Core || (Cydc.Core = {}));
+})(Cydc || (Cydc = {}));
---
+++ cydc/src/cydc/wwwroot/app/core/root.ts
@@ -0,0 +1,53 @@
+﻿namespace Cydc.Core {
+    export class RootCtrl {
+        static $inject = ["auth", "$location", "$mdSidenav", "$mdDialog"];
+        constructor(
+            private auth: Cydc.Auth,
+            private $location: ng.ILocationService,
+            private $mdSidenav: ng.material.ISidenavService,
+            private $mdDialog: ng.material.IDialogService
+        ) {
+            this.auth.refreshState();
+            window["root"] = this;
+        }
+
+        authState() {
+            return this.auth;
+        }
+
+        login($event) {
+            return this.$mdDialog.show({
+                controller: Cydc.Account.LoginCtrl,
+                controllerAs: "vm",
+                templateUrl: "/view/account/login.html",
+                parent: angular.element(document.body),
+                targetEvent: $event,
+                clickOutsideToClose: true
+            });
+        }
+
+        logout($event) {
+            this.$mdDialog.show(this.$mdDialog.confirm()
+                .parent(angular.element(document.body))
+                .clickOutsideToClose(true)
+                .title("确定要退出吗？")
+                .ariaLabel("Logout Dialog")
+                .targetEvent($event)
+                .ok("确定")
+                .cancel("取消")
+            ).then(() => {
+                return this.auth.logout();
+            }).then(() => {
+                this.$mdDialog.show(this.$mdDialog.alert()
+                    .title("退出成功")
+                    .targetEvent($event)
+                    .ariaLabel("Logout Success")
+                    .ok("知道了"));
+            });
+        }
+
+        openMenu() {
+            this.$mdSidenav("left").toggle();
+        }
+    }
+}
